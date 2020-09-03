a22d83bb389752f712686bf12d7082a4279c72be
EC2 Default User <sandeep.bsr007@gmail.com>
2020-09-03 18:52:31 +0000

    Reduce a number to 0 in minumum steps

---
+++ minimumsteps.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Reduce a number to 0 in minumum steps
+//1) Decrease the number by 1, or
+//2) Replace the number by its largest prime divisor(not 1 or itself)
+// Example: 9 to 0
+// 9
